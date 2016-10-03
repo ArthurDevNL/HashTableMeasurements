38618e3f4e720d424d3dc166f8b553d49b114c47
Gert-Jan van Ginkel <gertjanvg@gmail.com>
2016-10-03 11:28:53 +0200

    Create project

---
+++ HashTables/HashTables/Hashfunctions.hpp
@@ -0,0 +1,14 @@
+//
+//  Hashfunctions.hpp
+//  HashTables
+//
+//  Created by Gert-Jan van Ginkel on 03/10/16.
+//  Copyright © 2016 Gert-Jan van Ginkel. All rights reserved.
+//
+
+#ifndef Hashfunctions_hpp
+#define Hashfunctions_hpp
+
+#include <stdio.h>
+
+#endif /* Hashfunctions_hpp */
---
+++ HashTables/HashTables/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HashTables
+//
+//  Created by Gert-Jan van Ginkel on 03/10/16.
+//  Copyright © 2016 Gert-Jan van Ginkel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
